d4974f9674aeb9fd9290e02d40257738ccab107c
CardosoEdson <cardosoedson72@gmail.com>
2026-09-15 11:47:40 -0300

    atividades 1.0

---
+++ Ano_Bissexto/Ano_Bissexto/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ano_Bissexto
+{
+    internal class Program
+    { /*
+             6.Ano Bissexto
+            Determine se um ano é bissexto. Um ano é bissexto se for divisivel po 4, mas nao por 100, a nao ser que seja divisivel por 400.
+            .entrada :2024
+            .saida esperada: Ano bissexto */ 
+        static void Main(string[] args) 
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(@"
+░█████╗░███╗░░██╗░█████╗░  ██████╗░██╗░██████╗░██████╗███████╗██╗░░██╗████████╗░█████╗░
+██╔══██╗████╗░██║██╔══██╗  ██╔══██╗██║██╔════╝██╔════╝██╔════╝╚██╗██╔╝╚══██╔══╝██╔══██╗
+███████║██╔██╗██║██║░░██║  ██████╦╝██║╚█████╗░╚█████╗░█████╗░░░╚███╔╝░░░░██║░░░██║░░██║
+██╔══██║██║╚████║██║░░██║  ██╔══██╗██║░╚═══██╗░╚═══██╗██╔══╝░░░██╔██╗░░░░██║░░░██║░░██║
+██║░░██║██║░╚███║╚█████╔╝  ██████╦╝██║██████╔╝██████╔╝███████╗██╔╝╚██╗░░░██║░░░╚█████╔╝
+╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░  ╚═════╝░╚═╝╚═════╝░╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░ ");
+            Console.ResetColor();
+            int ano;
+
+            Console.WriteLine("escreva esse ano é bissexto");
+            ano = int.Parse(Console.ReadLine ());
+
+            if (ano % 4 == 0 && ano % 100 != 0)
+            {
+                Console.WriteLine(" Ano bissexto ");
+            }
+           else if(ano % 400 == 0)
+            {
+                Console.WriteLine(" Ano bissexto ");
+            }
+            else
+            {
+                Console.WriteLine(" não bissexto ");
+            }
+               
+
+
+           
+
+
+
+
+        }
+    }
+}
---
+++ Estoque_de_vinho/Estoque_de_vinho/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque_de_vinho
+{
+    internal class Program
+    { /* Construa um programa que permita fazer o levantamanto do estoque de vinhos de uma adega, tendo como dados de entrada
+       * tipos de vinho, sendo: "T" para tinto e "B" para branco. como a qauntidade de vinhos é desconhecida, utilize a leta " F"
+       * para finalizar.Após finalizar o programa deve mostrar a quantidade de cada vinho na adega */
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(@"
+░█████╗░██████╗░███████╗░██████╗░░█████╗░  ██████╗░███████╗  ██╗░░░██╗██╗███╗░░██╗██╗░░██╗░█████╗░░██████╗
+██╔══██╗██╔══██╗██╔════╝██╔════╝░██╔══██╗  ██╔══██╗██╔════╝  ██║░░░██║██║████╗░██║██║░░██║██╔══██╗██╔════╝
+███████║██║░░██║█████╗░░██║░░██╗░███████║  ██║░░██║█████╗░░  ╚██╗░██╔╝██║██╔██╗██║███████║██║░░██║╚█████╗░
+██╔══██║██║░░██║██╔══╝░░██║░░╚██╗██╔══██║  ██║░░██║██╔══╝░░  ░╚████╔╝░██║██║╚████║██╔══██║██║░░██║░╚═══██╗
+██║░░██║██████╔╝███████╗╚██████╔╝██║░░██║  ██████╔╝███████╗  ░░╚██╔╝░░██║██║░╚███║██║░░██║╚█████╔╝██████╔╝
+╚═╝░░╚═╝╚═════╝░╚══════╝░╚═════╝░╚═╝░░╚═╝  ╚═════╝░╚══════╝  ░░░╚═╝░░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝░╚════╝░╚═════╝░");
+            Console.ResetColor();
+            string tipos;
+            int tinto = 0, branco = 0;
+
+            Console.WriteLine(" Digite o tipo do vinho ( T para tinto, B para branco, F para Finalizar): ");
+            tipos = Console.ReadLine();
+
+
+
+
+            while (tipos != "f")
+            {
+                Console.WriteLine(" Programa Finalizado ");
+
+                if (tipos == "t")
+                {
+                    tinto++;
+                }
+                else if (tipos == "b")
+                {
+                    branco++;
+                }
+                Console.WriteLine("Digite o próximo vinho  ou f pra finalizar ):");
+                tipos = Console.ReadLine();
+            }
+
+            Console.WriteLine("Quantidade de vinho Tinto: " + tinto);
+            Console.WriteLine("Quantidade de vinho Branco: " + branco);
+        
+
+        }
+    }
+}
